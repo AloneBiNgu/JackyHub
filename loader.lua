f5e6d288254ae47986e2aa34cc8725c955a9eaa7
Jacky <thanhbesthresh@gmail.com>
2024-12-29 00:12:58 +0700

    Add files via upload

---
+++ loader.lua
@@ -0,0 +1,9 @@
+local s, source = pcall(function()
+    return game:HttpGet(('https://raw.githubusercontent.com/AloneBiNgu/JackyHub/refs/heads/main/%s'):format(tostring(game.PlaceId))
+end)
+
+if not source then
+    game.Players.LocalPlayer:Kick('[Jacky Hub] This game hasn\'t supported yet.')
+end
+
+loadstring(source)()
