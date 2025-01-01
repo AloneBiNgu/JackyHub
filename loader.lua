0f91c5e7c95b4045c08d00fce76087d7119c788e
Jacky <thanhbesthresh@gmail.com>
2025-01-02 02:02:22 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,9 +1,26 @@
-local s, source = pcall(function()
-    return game:HttpGet(('https://raw.githubusercontent.com/AloneBiNgu/JackyHub/refs/heads/main/%s'):format(tostring(game.PlaceId)))
-end)
+do
+    local list = {
+        3808223175
+    }
+    
+    local path = nil
+    for i, v in next, list do
+        if type(v) == 'table' then
+            path = v[table.find(v, game.PlaceId)]
+        end
+    end
 
-if not source then
-    game.Players.LocalPlayer:Kick('[Jacky Hub] This game hasn\'t supported yet.')
+    if path == nil then
+        path = list[table.find(list, game.GameId)]
+    end
+
+    local s, source = pcall(function()
+        return game:HttpGet(('https://raw.githubusercontent.com/AloneBiNgu/JackyHub/refs/heads/main/%s'):format(path))
+    end)
+    
+    if not source then
+        game.Players.LocalPlayer:Kick('[Jacky Hub] This game hasn\'t supported yet.')
+    end
+    
+    loadstring(source)()
 end
-
-loadstring(source)()
